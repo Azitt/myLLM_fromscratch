022d2485f3a39a52977106e8754398d5e05daf30
Kowalczyk <uidn0764@contiwan.com>
2024-05-18 23:45:28 -0400

    self

--- self_attention.py
+++ self_attention.py
@@ -1,4 +1,22 @@
+import torch.nn as nn
 import torch
+class SelfAttention_v1(nn.Module):
+    def __init__(self, d_in, d_out):
+        super().__init__()
+        self.d_out = d_out
+        self.W_query = nn.Parameter(torch.rand(d_in, d_out))
+        self.W_key = nn.Parameter(torch.rand(d_in, d_out))
+        self.W_value = nn.Parameter(torch.rand(d_in, d_out))
+    def forward(self, x):
+        keys = x @ self.W_key
+        queries = x @ self.W_query
+        values = x @ self.W_value
+        attn_scores = queries @ keys.T # omega
+        attn_weights = torch.softmax(
+        attn_scores / keys.shape[-1]**0.5, dim=-1)
+        context_vec = attn_weights @ values
+        return context_vec
+
 ## small input sample##############################################
 inputs = torch.tensor(
 [[0.43, 0.15, 0.89], # Your (x^1)
@@ -8,49 +26,34 @@ inputs = torch.tensor(
 [0.77, 0.25, 0.10], # one (x^5)
 [0.05, 0.80, 0.55]] # step (x^6)
 )
-## Step1 (computing attention score) suppose we have x(2) as a query => determine attention scores by computing the dot product of the query, x(2), with every other input token####
-query = inputs[1] #A
-attn_scores_2 = torch.empty(inputs.shape[0])
-for i, x_i in enumerate(inputs):
- attn_scores_2[i] = torch.dot(x_i, query)
-print(attn_scores_2)
+## in GPT-like models, the input and output dimensions##########
+x_2 = inputs[1] # The second input element
+d_in = inputs.shape[1] # The input embedding size, d=3
+d_out = 2  # The output embedding size, d_out=2
+##########
+torch.manual_seed(123)
+sa_v1 = SelfAttention_v1(d_in, d_out)
+print(sa_v1(inputs))
 
-## Step2 Normalization (compute attention weights) ###############################################
-attn_weights_2_tmp = attn_scores_2 / attn_scores_2.sum()
-print("Attention weights:", attn_weights_2_tmp)
-print("Sum:", attn_weights_2_tmp.sum())
+# use nn.Linear layers to effectively perform matrix multiplication when the bias units################################
+# are disabled. Also nn.Linear has an optimized weight initialization scheme, contributing to more stable and effective model training.    
 
-## normalization using softmax################
-def softmax_naive(x):
- return torch.exp(x) / torch.exp(x).sum(dim=0)
-attn_weights_2_naive = softmax_naive(attn_scores_2)
-print("Attention weights:", attn_weights_2_naive)
-print("Sum:", attn_weights_2_naive.sum())
+class SelfAttention_v2(nn.Module):
+    def __init__(self, d_in, d_out, qkv_bias=False):
+        super().__init__()
+        self.d_out = d_out
+        self.W_query = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_key = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_value = nn.Linear(d_in, d_out, bias=qkv_bias)
+    def forward(self, x):
+        keys = self.W_key(x)
+        queries = self.W_query(x)
+        values = self.W_value(x)
+        attn_scores = queries @ keys.T
+        attn_weights = torch.softmax(attn_scores / keys.shape[-1]**0.5, dim=1)
+        context_vec = attn_weights @ values
+        return context_vec
 
-## pytorch implementatopn of softmax##############
-attn_weights_2 = torch.softmax(attn_scores_2, dim=0)
-print("Attention weights:", attn_weights_2)
-print("Sum:", attn_weights_2.sum())
-
-## Step3 compute the context vector z (2) (multiplying each input vector by its corresponding attention weight)###############################
-query = inputs[1] # 2nd input token is the query
-context_vec_2 = torch.zeros(query.shape)
-for i,x_i in enumerate(inputs):
- context_vec_2 += attn_weights_2[i]*x_i
-print(context_vec_2)
-
-### attention weights for all input tokens#########################
-##step1 ################################
-attn_scores = torch.empty(6, 6)
-for i, x_i in enumerate(inputs):
- for j, x_j in enumerate(inputs):
-   attn_scores[i, j] = torch.dot(x_i, x_j)
-print(attn_scores)
-
-## step1 with matrix multiplication######################
-attn_scores = inputs @ inputs.T
-print(attn_scores)
-
-##Step2 Normalization ##################################
-attn_weights = torch.softmax(attn_scores, dim=1)
-print(attn_weights)
+torch.manual_seed(789)
+sa_v2 = SelfAttention_v2(d_in, d_out)
+print(sa_v2(inputs))    
