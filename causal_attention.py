e88fa74b0ee7e0f7dcc98b61701077d380310b57
Kowalczyk <uidn0764@contiwan.com>
2024-05-19 21:43:30 -0400

    causal

---
+++ causal_attention.py
@@ -0,0 +1,105 @@
+import torch.nn as nn
+import torch
+class SelfAttention_v1(nn.Module):
+    def __init__(self, d_in, d_out):
+        super().__init__()
+        self.d_out = d_out
+        self.W_query = nn.Parameter(torch.rand(d_in, d_out))
+        self.W_key = nn.Parameter(torch.rand(d_in, d_out))
+        self.W_value = nn.Parameter(torch.rand(d_in, d_out))
+    def forward(self, x):
+        keys = x @ self.W_key
+        queries = x @ self.W_query
+        values = x @ self.W_value
+        attn_scores = queries @ keys.T # omega
+        attn_weights = torch.softmax(
+        attn_scores / keys.shape[-1]**0.5, dim=-1)
+        context_vec = attn_weights @ values
+        return context_vec
+
+## small input sample##############################################
+inputs = torch.tensor(
+[[0.43, 0.15, 0.89], # Your (x^1)
+[0.55, 0.87, 0.66], # journey (x^2)
+[0.57, 0.85, 0.64], # starts (x^3)
+[0.22, 0.58, 0.33], # with (x^4)
+[0.77, 0.25, 0.10], # one (x^5)
+[0.05, 0.80, 0.55]] # step (x^6)
+)
+## in GPT-like models, the input and output dimensions##########
+x_2 = inputs[1] # The second input element
+d_in = inputs.shape[1] # The input embedding size, d=3
+d_out = 2  # The output embedding size, d_out=2
+##########
+torch.manual_seed(123)
+sa_v1 = SelfAttention_v1(d_in, d_out)
+# print(sa_v1(inputs))
+
+# use nn.Linear layers to effectively perform matrix multiplication when the bias units################################
+# are disabled. Also nn.Linear has an optimized weight initialization scheme, contributing to more stable and effective model training.    
+
+class SelfAttention_v2(nn.Module):
+    def __init__(self, d_in, d_out, qkv_bias=False):
+        super().__init__()
+        self.d_out = d_out
+        self.W_query = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_key = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_value = nn.Linear(d_in, d_out, bias=qkv_bias)
+    def forward(self, x):  # this is the same as __call__ function
+        keys = self.W_key(x)
+        queries = self.W_query(x)
+        values = self.W_value(x)
+        attn_scores = queries @ keys.T
+        attn_weights = torch.softmax(attn_scores / keys.shape[-1]**0.5, dim=1)
+        context_vec = attn_weights @ values
+        return context_vec
+
+torch.manual_seed(789)
+sa_v2 = SelfAttention_v2(d_in, d_out)
+# print(sa_v2(inputs))    
+
+##causal attention##################################################################
+## step1 create attention weights############
+queries = sa_v2.W_query(inputs) #A
+keys = sa_v2.W_key(inputs)
+attn_scores = queries @ keys.T
+attn_weights = torch.softmax(attn_scores / keys.shape[-1]**0.5, dim=1)
+print(attn_weights)
+
+## step2 create a mask#######################
+context_length = attn_scores.shape[0]
+mask_simple = torch.tril(torch.ones(context_length, context_length))
+print(mask_simple)
+
+masked_simple = attn_weights*mask_simple
+print(masked_simple)
+
+## step3 renormalize the attention weights to sum up to 1#####################
+row_sums = masked_simple.sum(dim=1, keepdim=True)
+masked_simple_norm = masked_simple / row_sums
+print(masked_simple_norm)
+
+## more effective way to implement causal ##################################################
+## step 1 mask with -inf#############
+mask = torch.triu(torch.ones(context_length, context_length), diagonal=1)
+masked = attn_scores.masked_fill(mask.bool(), -torch.inf)
+print(masked)
+
+## step2 apply softmax############
+attn_weights = torch.softmax(masked / keys.shape[-1]**0.5, dim=1)
+print(attn_weights)
+
+## step3 50 percent dropout to weights##############
+torch.manual_seed(123)
+dropout = torch.nn.Dropout(0.5)
+print(dropout(attn_weights))
+
+## step4 context_vector######################
+# context_vec_2 = attn_weights @ values
+# print(context_vec_2)
+
+## droput of 50 rate for demo#################
+torch.manual_seed(123)
+dropout = torch.nn.Dropout(0.5) #A
+example = torch.ones(6, 6) #B
+print(dropout(example))
